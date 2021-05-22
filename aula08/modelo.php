88662bbe35e7bfb23bd30bf60999cde27e0b48ef
NandoCruz <nando33_2005@hotmail.com>
2021-05-21 22:23:18 -0300

    aula 08
    criando a pasta da aula 08 de PHP

---
+++ aula08/modelo.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8"/>
+    <title>Primeiro Exemplo PHP</title>
+    <link rel="stylesheet" href="_css/estilo.css"/>
+    <style>
+        h1{
+            color: #3c5ab4;
+            text-shadow: 1px 1px 1px black;
+            text-align: center;
+        }
+    </style>
+</head>
+<body>
+<div>
+    <h1>Operações Aritméticas</h1>
+    <?php
+
+
+    ?>
+</div>
+</body>
+</html>
