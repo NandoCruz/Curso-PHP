e7a06f2308443601db5cc924100f43bc0dfdfe1f
NandoCruz <nando33_2005@hotmail.com>
2021-05-23 22:48:16 -0300

    projeto 04 contar PHP
    Projeto usando if e while para fazer incremento e decremento.

--- aula10/04-contar.php
+++ aula10/04-contar.php
@@ -3,11 +3,11 @@
 <head>
     <?php
         $n1 = isset($_GET["n1"]) ? $_GET["n1"] : 0;
-        $n2 = isset($_GET["n2"]) ? $_GET["n2"] : 0;
-        $n3 = isset($_GET["i"]) ? $_GET["i"] : 0;
+        $n2 = isset($_GET["n2"]) ? $_GET["n2"] : 1;
+        $n3 = isset($_GET["n3"]) ? $_GET["n3"] : 0;
     ?>
     <meta charset="UTF-8"/>
-    <title>Primeiro Exemplo PHP</title>
+    <title>Contar até... PHP</title>
     <link rel="stylesheet" href="_css/estilo.css"/>
     <style>
         h1{
@@ -20,12 +20,21 @@
 <body>
 <div>
     <?php
-    echo "<h1>Tabuada de $n1</h1>";
-            $n3 = 1;
-            while ($n3 <= 10){
-                echo "$n1 x $n2 <br>";
+            if ($n1 < $n2){
+                echo "<h1>Contar de $n1 até $n2</h1>";
+                echo "$n1 é menor que $n2 e vou pular $n3<br>";
+                while ($n1 <= $n2){
+                    echo "$n1 ";
+                    $n1 += $n3;
+                }
 
-                 $n3 ++;
+            } else {
+                echo "<h1>Contar de $n1 até $n2</h1>";
+                echo "$n1 é maior que $n2 e vou pular $n3<br>";
+                while ($n1 >= $n2){
+                    echo "$n1 ";
+                    $n1 -= $n3;
+                }
             }
     ?>
     <br><a href="javascript:history.go(-1)" class="botao" >Voltar</a>
